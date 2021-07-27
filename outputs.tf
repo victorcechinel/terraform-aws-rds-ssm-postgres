3d47d663c61187b9292985e932f174a33c6788ca
Victor Cechinel <dev.victorcechinel@gmail.com>
2021-07-27 03:50:50 -0300

    :arrow_up: #1 - Update terraform version
    Update terraform version

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 output "rds_postgres_endpoint" {
-  value = aws_db_instance.rds_postgres.endpoint
+  value       = aws_db_instance.rds_postgres.endpoint
+  description = "RDS endpoint."
 }
 
 output "rds_postgres_username" {
-  value = aws_db_instance.rds_postgres.username
+  value       = aws_db_instance.rds_postgres.username
+  description = "RDS username."
 }
 
 output "rds_postgres_password" {
-  value = aws_db_instance.rds_postgres.password
+  value       = aws_db_instance.rds_postgres.password
+  sensitive   = true
+  description = "RDS password."
 }
