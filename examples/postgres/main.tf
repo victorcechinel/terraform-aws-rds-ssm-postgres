0e61c25fd780358b833c6e4b59917a263ec02913
Victor Cechinel <dev.victorcechinel@gmail.com>
2021-07-27 03:41:06 -0300

    :sparkles: #1 - Update terraform examples
    Update terraform example

--- examples/postgres/main.tf
+++ examples/postgres/main.tf
@@ -1,9 +1,13 @@
 module "aws_rds_postgres" {
   source  = "victorcechinel/rds-ssm-postgres/aws"
-  version = "1.0.5"
+  version = "1.0.6"
 
   identifier             = "rds-identifier"
   subnet_group           = "my-subnet-group"
   parameter_group        = "my-parameter-group"
-  vpc_security_group_ids = ["vpc-group-id"]
+  vpc_security_group_ids = ["vpc-security-group-ids"]
+
+  tags = {
+    Environment = "production"
+  }
 }
--- examples/postgres/versions.tf
+++ examples/postgres/versions.tf
@@ -1,15 +1,14 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.51.0"
     }
-
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.1.0"
     }
   }
 }
